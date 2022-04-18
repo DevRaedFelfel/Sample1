f6a910fd41ee2309c2185a2971d83b8788ed75b2
Raed Felfel <raed.felfel@hp.com>
2022-04-18 13:02:39 +0300

    comments

--- src/app/interceptors/progress.interceptor.ts
+++ src/app/interceptors/progress.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { finalize, Observable } from 'rxjs';
+import { finalize, Observable, tap } from 'rxjs';
 import { ProgressService } from '../services/progress.service';
 
 @Injectable()
@@ -16,9 +16,16 @@ export class ProgressInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log("pushing");
     this.progressService.PushRequest();
-    return next.handle(request).pipe(
-      finalize(() => this.progressService.PopRequest())
+    // it has 3 reasons
+    // 1- to call the next intercept function inside the next interceptor
+    // 2- in case not next interceptor. it executes the actual HttpClient verb. such as the get
+    // 3- then after complete the execution of this request, execute the call back
+    return next.handle(request).pipe (
+      finalize(() => this.progressService.PopRequest()),
     );
 
   }
 }
+
+// inside the header add the authorize header with token
+// for each request using a new created interceptor.
